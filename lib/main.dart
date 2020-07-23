1e07356b858572c00de5faf6337125effbff8b65
mmalak7 <marcinmalak7@gmail.com>
2020-07-23 14:11:37 +0200

    specify navigator & design tasks page

--- lib/main.dart
+++ lib/main.dart
@@ -65,7 +65,7 @@ class MainPage extends StatelessWidget {
               ),
               onPressed: () {
                 Navigator.push(context,
-                    MaterialPageRoute(builder: (context) => CreateTaskPage()));
+                    MaterialPageRoute(builder: (context) => TaskPage()));
               },
             )
           ],
@@ -75,23 +75,138 @@ class MainPage extends StatelessWidget {
   }
 }
 
-class CreateTaskPage extends StatelessWidget {
+class TaskPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Color(0XFF6747ce),
+      body: SafeArea(
+        child: Container(
+          child: Padding(
+            padding: const EdgeInsets.all(24.0),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: <Widget>[
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: <Widget>[
+                    Ink(
+                      width: 36.0,
+                      height: 36.0,
+                      decoration: const ShapeDecoration(
+                        color: Color(0XFF7e60db),
+                        shape: CircleBorder(),
+                      ),
+                      child: IconButton(
+                        iconSize: 18.0,
+                        icon: Icon(Icons.arrow_back),
+                        color: Colors.white,
+                        onPressed: () {
+                          Navigator.push(
+                              context,
+                              MaterialPageRoute(
+                                  builder: (context) => MainPage()));
+                        },
+                      ),
+                    ),
+                    Text(
+                      'today\'s tasks',
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 20.0,
+                      ),
+                    ),
+                    Ink(
+                      width: 36.0,
+                      height: 36.0,
+                      decoration: const ShapeDecoration(
+                        color: Color(0XFF7e60db),
+                        shape: CircleBorder(),
+                      ),
+                      child: IconButton(
+                        iconSize: 18.0,
+                        icon: Icon(Icons.done_all),
+                        color: Colors.white,
+                        onPressed: () {
+                          Navigator.push(
+                              context,
+                              MaterialPageRoute(
+                                  builder: (context) => MainPage()));
+                        },
+                      ),
+                    ),
+                  ],
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    RaisedButton(
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(26.0),
+                      ),
+                      padding: EdgeInsets.all(16.0),
+                      color: Colors.white,
+                      child: Text(
+                        'Create task',
+                        style: TextStyle(
+                          fontSize: 16,
+                          letterSpacing: 2,
+                        ),
+                      ),
+                      onPressed: () {
+                        Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                                builder: (context) => CreateTaskPage()));
+                      },
+                    )
+                  ],
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
     );
   }
 }
 
-// class CreateTaskPage extends StatefulWidget {
-//   @override
-//   _CreateTaskPageState createState() => _CreateTaskPageState();
-// }
+class CreateTaskPage extends StatefulWidget {
+  @override
+  _CreateTaskPageState createState() => _CreateTaskPageState();
+}
 
-// class _CreateTaskPageState extends State<CreateTaskPage> {
-//   @override
-//   Widget build(BuildContext context) {
-//     return Container();
-//   }
-// }
+class _CreateTaskPageState extends State<CreateTaskPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Color(0XFF6747ce),
+      body: Container(
+        alignment: Alignment.center,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            RaisedButton(
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(26.0),
+              ),
+              padding: EdgeInsets.all(16.0),
+              color: Colors.white,
+              child: Text(
+                'Back to all tasks',
+                style: TextStyle(
+                  fontSize: 16,
+                  letterSpacing: 2,
+                ),
+              ),
+              onPressed: () {
+                Navigator.push(context,
+                    MaterialPageRoute(builder: (context) => TaskPage()));
+              },
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
